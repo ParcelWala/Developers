8fe30bf2c6e8dede6fe14c0571d7979682226449
Administrator <Administrator@DESKTOP-FI1EJ2S>
2025-11-20 22:01:48 +0530

    Update the connectionstring

---
+++ ParcelwalaAPP.DataAccess/DTOs/RefreshToken.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcelwalaAPP.DataAccess.DTOs
+{
+    public class RefreshTokenRequest
+    {
+        [Required(ErrorMessage = "Refresh token is required")]
+        public string RefreshToken { get; set; } = string.Empty;
+    }
+
+    public class RefreshTokenResponse
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public TokenData? Data { get; set; }
+    }
+
+    public class TokenData
+    {
+        public string AccessToken { get; set; } = string.Empty;
+        public int ExpiresIn { get; set; }
+    }
+}
